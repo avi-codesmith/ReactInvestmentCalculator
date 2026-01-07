04e0e33cb3669d81fbb1efc325eeb32720b0f52a
avi-codesmith <avi.codesmith@gmail.com>
2026-01-07 16:46:42 +0530

    Two way binding

--- src/component/userInput.jsx
+++ src/component/userInput.jsx
@@ -1,32 +1,67 @@
 import { useState } from "react";
 
 export default function UserInput() {
-  useState({
+  const [userInput, setUserInput] = useState({
     initialInvestment: 1000,
     annualInvestment: 10000,
     expectedReturn: 6,
     duration: 10,
   });
+
+  function handleInput(inputIdentifier, newValue) {
+    setUserInput((prev) => ({
+      ...prev,
+      [inputIdentifier]: newValue,
+    }));
+  }
+
   return (
     <div id="user-input">
-      <div class="input-group ">
+      <div className="input-group">
         <div>
           <label>initial investment</label>
-          <input />
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(event) =>
+              handleInput("initialInvestment", event.target.value)
+            }
+          />
         </div>
         <div>
           <label>annual investment</label>
-          <input />
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(event) =>
+              handleInput("annualInvestment", event.target.value)
+            }
+          />
         </div>
       </div>
-      <div class="input-group ">
+
+      <div className="input-group">
         <div>
-          <label> expected return</label>
-          <input />
+          <label>expected return</label>
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(event) =>
+              handleInput("expectedReturn", event.target.value)
+            }
+          />
         </div>
         <div>
           <label>duration</label>
-          <input />
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(event) => handleInput("duration", event.target.value)}
+          />
         </div>
       </div>
     </div>
